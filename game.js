b6be97e9cf82d9f17bbf6d61cae41aadf2765407
Dom <Sanek.s88@gmail.com>
2020-10-26 16:12:08 +0300

    26.10

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
 let game = {
-    start: function() {
+    ctx:null,
+    background:null,
+    ball:null,
+    platform:null,
+    //инициализация
+    init:function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
-        let background = new Image();
-        background.src = "img/background.png";
+    },
+    // загрузка спрайтов
+    preload:function() {
+        this.background = new Image();
+        this.background.src = "img/background.png";
+
+        this.ball = new Image();
+        this.ball.src = "img/ball.png";
+
+        this.platform = new Image();
+        this.platform.src = "img/platform.png";
+    },
+    // запуск
+    run:function() {
         window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0);
+          this.render();
         });
+    },
+    //отрисовка
+    render: function (){
+        this.ctx.drawImage(this.background, 0, 0);
+        this.ctx.drawImage(this.ball, 0, 0);
+        this.ctx.drawImage(this.platform, 100,100);
+    },
+    start: function() {
+        this.init();
+        this.preload();
+        this.run();
     }
 };
 
