e4062a482ffd5c32953e226187c11c6b583a4bbc
Dom <Sanek.s88@gmail.com>
2020-10-26 18:25:37 +0300

    26.10/4

--- game.js
+++ game.js
@@ -2,10 +2,15 @@ let game = {
     ctx:null,
     platform:null,
     ball:null,
+    block:null,
+    blocks:[],
+    rows: 4,
+    cols: 8,
     sprites:{
         background:null,
         ball:null,
         platform:null,
+        block:null
     },
     //инициализация
     init:function() {
@@ -15,15 +20,29 @@ let game = {
     preload:function(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
+        let onImageLoad = () => {
+            ++loaded;
+            if(loaded >= required){
+                callback();
+            }
+        };
+
         for(let key in this.sprites){
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/"+ key + ".png";
-            this.sprites[key].addEventListener("load", () => {
-                ++loaded;
-                if(loaded >= required){
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener("load",onImageLoad);
+        }
+    },
+    //Создание блоков
+    create: function (){
+        for (let row = 0 ; row < this.rows; row++){
+            for (let col = 0; col < this.cols; col++){
+                this.blocks.push({
+                    x: 72 * col +25,
+                    y: 27 * row +10,
+                });
+
+            }
         }
     },
     // запуск
@@ -37,25 +56,36 @@ let game = {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, this.ball.x, this.ball.y,this.ball.w,this.ball.h);
         this.ctx.drawImage(this.sprites.platform, this.platform.x,this.platform.y,this.platform.w,this.platform.h);
+        for( let block of this.blocks){
+            console.log(this.blocks);
+            this.ctx.drawImage(this.sprites.block, block.x,block.y,this.block.w,this.block.h);
+        }
     },
     start: function() {
         this.init();
         this.preload(()=>{
+            this.create();
             this.run();
         });
     }
 };
+game.block ={
+    x:0,
+    y:0,
+    w:70, //Длинна
+    h:25, //Высота
+};
 game.ball ={
     x:280,
     y:270,
-    w:30,
-    h:30,
+    w:30, //Длинна
+    h:30, //Высота
 };
 game.platform ={
    x:220,
    y:300,
-   w:150,
-   h:30,
+   w:150, //Длинна
+   h:30,  //Высота
 };
 window.addEventListener("load", () => {
     game.start();
